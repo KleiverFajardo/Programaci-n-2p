980c859c85e7be8112023d2be7e5b9e4ad745c44
KleiverFajardo <161411817+KleiverFajardo@users.noreply.github.com>
2024-09-16 11:05:23 -0500

    Add files via upload

---
+++ Actividad2 TecnarGui/TecnarGuii.java
@@ -0,0 +1,24 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package tecnarguii;
+
+/**
+ *
+ * @author Biblioteca
+ */
+public class TecnarGuii {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+        
+        frmPersonas P = new frmPersonas();
+        P.setVisible(true);
+
+    }
+    
+}
